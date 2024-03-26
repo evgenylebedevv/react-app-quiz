b9fc41fc22dab4ddf389a13bcbb1c0e1a49a4a8f
evgeny <evgenylebedevv@gmail.com>
2024-03-26 10:43:38 +0300

    Four commit

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ const questions = [
 ];
 
 // Компонент для отображения результата
-function Result({ correct }) {
+function Result({correct}) {
     return (
         <div className="result">
-            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
             <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
             <a href="/">
                 <button>Попробовать снова</button>
@@ -39,17 +39,18 @@ function Result({ correct }) {
 }
 
 // Компонент для игры
-function Game({ step, question, onClickVariant }) {
+function Game({step, question, onClickVariant}) {
     const percentage = Math.round((step / questions.length) * 100)
 
     return (
         <>
             <div className="progress">
-                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+                <div style={{width: `${percentage}%`}} className="progress__inner"></div>
             </div>
             <h1>{question.title}</h1>
             <ul>
-                {question.variants.map((text, index) => <li onClick={() => onClickVariant(index)} key={text}>{text}</li>)}
+                {question.variants.map((text, index) => <li onClick={() => onClickVariant(index)}
+                 key={text}>{text}</li>)}
             </ul>
         </>
     );
@@ -72,11 +73,9 @@ function App() {
     return (
         <div className="App">
             {step !== questions.length ?
-                (
-                    <Game step={step} question={question} onClickVariant={onClickVariant}/>) :
-                (
-                    <Result correct={correct}/>
-                )}
+                <Game step={step} question={question} onClickVariant={onClickVariant}/> :
+                <Result correct={correct}/>
+            }
         </div>
     );
 }
