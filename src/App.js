a759bc40f22c756313df870ca9b40918d9b0de8a
evgeny <evgenylebedevv@gmail.com>
2024-03-26 09:13:31 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,67 @@
+// Импорт стилей для приложения
 import './index.scss';
 import React from "react";
 
+// Массив вопросов с ответами
 const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
+    {
+        title: 'React - это ... ?',
+        variants: ['библиотека', 'фреймворк', 'приложение'],
+        correct: 0,
+    },
+    {
+        title: 'Компонент - это ... ',
+        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+        correct: 1,
+    },
+    {
+        title: 'Что такое JSX?',
+        variants: [
+            'Это простой HTML',
+            'Это функция',
+            'Это тот же HTML, но с возможностью выполнять JS-код',
+        ],
+        correct: 2,
+    },
 ];
 
+// Компонент для отображения результата
 function Result({ correct }) {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <a href="/">
-          <button>Попробовать снова</button>
-      </a>
-    </div>
-  );
+    return (
+        <div className="result">
+            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+            <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+            <a href="/">
+                <button>Попробовать снова</button>
+            </a>
+        </div>
+    );
 }
 
+// Компонент для игры
 function Game({ step, question, onClickVariant }) {
     const percentage = Math.round((step / questions.length) * 100)
-    console.log(percentage)
 
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-      </div>
-      <h1>{question.title}</h1>
-      <ul>
-          {question.variants.map((text, index) => <li onClick={() => onClickVariant(index)} key={text}>{text}</li>)}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <div className="progress">
+                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+                {question.variants.map((text, index) => <li onClick={() => onClickVariant(index)} key={text}>{text}</li>)}
+            </ul>
+        </>
+    );
 }
 
+// Основной компонент приложения
 function App() {
     const [step, setStep] = React.useState(0)
     const [correct, setCorrect] = React.useState(0)
-    const question = questions[step] //step - номер индекса массива
+    const question = questions[step] // step - номер индекса массива
 
-
+    // Обработчик клика по варианту ответа
     const onClickVariant = (index) => {
         setStep(step + 1)
         if (index === question.correct) {
@@ -65,18 +69,16 @@ function App() {
         }
     }
 
-  return (
-    <div className="App">
-        {step !== questions.length ?
-            (
-                <Game step={step} question={question} onClickVariant={onClickVariant}/>) :
-            (
-                <Result correct={correct}/>
-            )}
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+            {step !== questions.length ?
+                (
+                    <Game step={step} question={question} onClickVariant={onClickVariant}/>) :
+                (
+                    <Result correct={correct}/>
+                )}
+        </div>
+    );
 }
 
 export default App;
